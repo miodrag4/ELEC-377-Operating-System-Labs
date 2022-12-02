884745653c2a8f170b90382d0cb9b98b79db6f9e
Mile Stosic <100725670+miodrag4@users.noreply.github.com>
2022-12-02 15:44:57 -0500

    Delete lab3 directory

--- lab3/hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <pwd.h>
-
-int main(int argc, char * argv[]){
-    struct passwd *pw = getpwuid(getuid());
-    printf("Hello %s\n", pw->pw_name);
-}
